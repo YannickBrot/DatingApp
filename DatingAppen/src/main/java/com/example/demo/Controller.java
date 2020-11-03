25fb534117f1d3dc1c3223d183405e96127187a8
maxmeldal <70686398+maxmeldal@users.noreply.github.com>
2020-11-03 11:25:04 +0100

    connect to database function

--- DatingAppen/src/main/java/com/example/demo/Controller.java
+++ DatingAppen/src/main/java/com/example/demo/Controller.java
@@ -1,11 +1,20 @@
 package com.example.demo;
 
+import com.example.demo.JDBCWriter;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
 @org.springframework.stereotype.Controller
 public class Controller {
+    com.example.demo.JDBCWriter jdbcWriter = new JDBCWriter();
 
     @GetMapping
+    public String welcome(Model model){
+        model.addAttribute("Gonnect", jdbcWriter.setConnection());
+        return ("welcome");
+    }
+
+    @GetMapping ("/home")
     public String home(){
         return ("home");
     }
--- DatingAppen/src/main/java/com/example/demo/JDBCWriter.java
+++ DatingAppen/src/main/java/com/example/demo/JDBCWriter.java
@@ -1,4 +1,4 @@
-package demo;
+package com.example.demo;
 
 import java.sql.*;
 
