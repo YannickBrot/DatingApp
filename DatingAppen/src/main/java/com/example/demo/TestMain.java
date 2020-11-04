566c3388b535a317e991179e9a6f1634222edea3
elmacbook <yannickbrot@gmail.com>
2020-11-04 10:17:32 +0100

    Test calculateAge

---
+++ DatingAppen/src/main/java/com/example/demo/TestMain.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+
+import Objects.User;
+
+import java.time.LocalDate;
+
+public class TestMain {
+
+    public static void main(String[] args){
+        User bob = new User(12,"Bob","Hansen", LocalDate.of(1993, 12,12), "bob@Hansen.dk",22222222, "Description","photo","bob","male","gay" );
+
+    }
+}
---
+++ DatingAppen/src/test/java/Objects/UserTest.java
@@ -0,0 +1,22 @@
+package Objects;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+    public class AgeCalculatorTest {
+
+
+        @Test
+        public void testCalculateAge_Success() {
+            // setup
+            LocalDate birthDate = LocalDate.of(1961, 5, 17);
+
+        }
+    }
+
+}
