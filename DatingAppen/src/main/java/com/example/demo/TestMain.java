aa94c54ce3977c2f36f8fb68aec6b4f92d6a8565
Yannick <yannickbrot@gmail.com>
2020-11-04 10:24:35 +0100

    Tested calculateAge

--- DatingAppen/src/main/java/com/example/demo/TestMain.java
+++ DatingAppen/src/main/java/com/example/demo/TestMain.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 public class TestMain {
 
     public static void main(String[] args){
-        User bob = new User(12,"Bob","Hansen", LocalDate.of(1993, 12,12), "bob@Hansen.dk",22222222, "Description","photo","bob","male","gay" );
+        User bob = new User(12,"Bob","Hanseen", LocalDate.of(1993, 12,12), "bob@Hansen.dk",22222222, "Description","photo","bob","male","gay" );
         System.out.println(bob.calculateAge());
     }
 }
